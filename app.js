d9776c3912500f2f9d6e1b7044482ebae5a2a173
Luis Garcia <74296603+luisgarcia96@users.noreply.github.com>
2022-03-18 12:37:41 +0100

    Inclusion de Bootstrap

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
+const ejsMate = require('ejs-mate');
 const {urlencoded} = require('express');
 const methodOverride = require('method-override');
 
@@ -19,9 +20,9 @@ const port = 3000
 
 
 //Configuration générale pour l'application Express
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
-
 
 app.use(urlencoded({extended: true}));
 app.use(methodOverride('_method'));

8ef232691707c0ad1025dfb3eb757b98e90d04a5
Luis Garcia <74296603+luisgarcia96@users.noreply.github.com>
2022-04-20 18:30:36 +0200

    Création des routes /campground

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const {urlencoded} = require('express');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 
+const campgrounds = require('./routes/campgrounds');
+
 //Connexion à la Base de données
 main().catch(err => console.log(err));
 
@@ -32,16 +34,6 @@ app.use(urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 //app.use(morgan('dev'));
 
-const validateCampground = (req, res, next) => {
-  
-  const {error} = campgroundSchema.validate(req.body);
-  if(error){
-    const msg = error.details.map(el => el.message).join(',');
-    throw new ExpressError(msg, 400);
-  } else {
-    next();
-  }
-}
 
 const validateReview = (req, res, next) => {
   const {error} = reviewSchema.validate(req.body);
@@ -53,61 +45,12 @@ const validateReview = (req, res, next) => {
   }
 }
 
-
+app.use('/campgrounds', campgrounds);
 
 app.get('/', (req, res) => {
   res.render('home.ejs');
 })
 
-app.get('/campgrounds', catchAsync(async (req, res) => {
-  const allCampgrounds = await Campground.find({});
-  res.render('campgrounds/index.ejs', {
-    allCampgrounds
-  });
-}))
-
-app.get('/campgrounds/new', (req, res) => {
-  res.render('campgrounds/new.ejs');
-})
-
-app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
-  // if (!req.body.campground) {
-  //   throw new ExpressError('Invalid campground data', 400);
-  // }
-  const newCampground = new Campground(req.body.campground);
-  await newCampground.save();
-  res.redirect(`/campgrounds/${newCampground._id}`)
-}))
-
-app.get('/campgrounds/:id', catchAsync(async (req, res) => {
-  const {id} = req.params;
-  const campground = await Campground.findById(id).populate('reviews');
-  res.render('campgrounds/show.ejs', {
-    campground
-  });
-}))
-
-app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
-  const {id} = req.params;
-  const campground = await Campground.findById(id);
-  res.render('campgrounds/edit.ejs', {
-    campground
-  });
-}))
-
-app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
-  const {id} = req.params;
-  const campground = await Campground.findByIdAndUpdate(id, {
-    ...req.body.campground
-  });
-  res.redirect(`/campgrounds/${campground._id}`);
-}))
-
-app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
-  const {id} = req.params;
-  await Campground.findByIdAndDelete(id);
-  res.redirect('/campgrounds');
-}))
 
 app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res) => {
   const {id} = req.params;

443b9a8c5e27e0d6aa4a85fa8fa759979e6b6ef2
Luis Garcia <luisarmandogarcia96@gmail.com>
2022-12-28 20:57:38 +0100

    Add coordinates to campground object

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,5 +1,8 @@
 const Campground = require('../models/campground');
 const { cloudinary } = require("../cloudinary");
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const mapBoxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({accessToken: mapBoxToken});
 
 module.exports.index = async (req, res) => {
   const allCampgrounds = await Campground.find({});
@@ -13,7 +16,12 @@ module.exports.renderNewForm = (req, res) => {
 }
 
 module.exports.createCampground = async (req, res, next) => {
+  const geoData = await geocoder.forwardGeocode({
+    query: req.body.campground.location,
+    limit: 1
+  }).send();
   const newCampground = new Campground(req.body.campground);
+  newCampground.geometry = geoData.body.features[0].geometry;
   newCampground.images = req.files.map(file => ({url: file.path, filename: file.filename}))
   newCampground.author = req.user._id;
   await newCampground.save();
